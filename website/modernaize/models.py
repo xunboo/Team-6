af38f60c8bc75158cf6076fd6383a5ad00fc4707
Aleksander Dash <adash@stanford.edu>
2019-05-04 18:55:49 -0700

    Minimum viable image upload functionality

--- website/modernaize/models.py
+++ website/modernaize/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class UploadedImage(models.Model):
+	image = models.ImageField(upload_to='uploaded_images', height_field='image_height', width_field='image_width')
+	image_width = models.PositiveIntegerField(editable=False)
+	image_height = models.PositiveIntegerField(editable=False)
+	uploaded_at = models.DateTimeField(auto_now_add=True)
