76b1d160fbb20e59e4c4fc90527af12b36a984a3
Aleksander Dash <adash@stanford.edu>
2019-05-08 13:01:29 -0700

    Split the training and test image upscaling directories

---
+++ upscaling_test/upscale_image.py
@@ -0,0 +1,32 @@
+import os, sys
+sys.path.insert(0, os.path.abspath('../upscaling/image-super-resolution/'))
+from ISR.models import RDN
+import numpy as np
+from PIL import Image
+import sys
+
+''' 
+	Expects local or direct path to image to upscale
+	returns a Pillow Image object
+'''
+def upscale_image(img_path):
+
+    img = Image.open(img_path)
+    img = img.convert('RGB')
+    lr_img = np.array(img)
+    assert(lr_img.shape[2] == 3)
+    rdn = RDN(arch_params={'C':6, 'D':20, 'G':64, 'G0':64, 'x':2})
+    rdn.model.load_weights('weights/rdn-C6-D20-G64-G064-x2_ArtefactCancelling_epoch219.hdf5')
+
+    sr_img = rdn.predict(lr_img)
+    return Image.fromarray(sr_img)
+
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print("Please input path to single image to upscale")
+    else:
+        img_path = sys.argv[1]
+        print("Upscaling image ", img_path)
+        img = upscale_image(img_path)
+        img.save(img_path[:-4] + "_sr.png")
