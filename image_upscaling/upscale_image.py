253936b3170df30f4ef989b50c348d5dface4012
Nolan Handali <nolanhandali@gmail.com>
2019-05-08 03:21:09 -0700

    fixed bugs in image upscaling code
    also upscaled the trocc

--- image_upscaling/upscale_image.py
+++ image_upscaling/upscale_image.py
@@ -9,19 +9,22 @@ import sys
 '''
 def upscale_image(img_path):
 
-	img = Image.open(img_path)
-	lr_img = np.array(img)
-	rdn = RDN(arch_params={'C':6, 'D':20, 'G':64, 'G0':64, 'x':2})
-	rdn.model.load_weights('image_upscaling/weights/rdn-C6-D20-G64-G064-x2_ArtefactCancelling_epoch219.hdf5')
+    img = Image.open(img_path)
+    img = img.convert('RGB')
+    lr_img = np.array(img)
+    assert(lr_img.shape[2] == 3)
+    rdn = RDN(arch_params={'C':6, 'D':20, 'G':64, 'G0':64, 'x':2})
+    rdn.model.load_weights('image-super-resolution/weights/rdn-C6-D20-G64-G064-x2_ArtefactCancelling_epoch219.hdf5')
 
-	sr_img = rdn.predict(lr_img)
-	return Image.fromarray(sr_img)
+    sr_img = rdn.predict(lr_img)
+    return Image.fromarray(sr_img)
 
 
 if __name__ == '__main__':
-	if len(sys.argv) != 2:
-		print("Please input path to single image to upscale")
-		return
-	img_path = sys.argv[2]
-	img = upscale_image(img_path)
-	img.save(img_path[:-3] + "_sr.png")
+    if len(sys.argv) != 2:
+        print("Please input path to single image to upscale")
+    else:
+        img_path = sys.argv[1]
+        print("Upscaling image ", img_path)
+        img = upscale_image(img_path)
+        img.save(img_path[:-4] + "_sr.png")
